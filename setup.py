e2d822c0d4043e28fa6dd95f57342a3364f8243b
toyg <g.lacava@gmail.com>
2012-10-21 03:32:59 +0100

    added setup.py and (empty) LICENSE to enable pip install from other trees

---
+++ setup.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+import os
+import sys
+
+import requests
+from requests.compat import is_py2
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    sys.exit()
+
+os.environ['PYTHONDONTWRITEBYTECODE'] = '1'
+
+packages = ['kippt_wrapper']
+
+requires = []
+
+description = ""
+with open('README.md','r') as readme:
+    description = readme.read()
+license = ""
+with open('LICENSE','r') as licenseFile:
+    license = licenseFile.read()
+
+
+setup(
+    name='Kippt Wrapper',
+    version="0.1",
+    description='Kippt.com API wrapper for Python',
+    long_description=description,
+    author='Thomas Biddle, Giacomo Lacava',
+    author_email='g.lacava@gmail.com',
+    url='https://github.com/toyg/Kippt-Python-Wrapper',
+    packages=packages,
+    package_data={'': ['LICENSE']},
+    package_dir={'kippt_wrapper': 'src'},
+    include_package_data=True,
+    install_requires=requires,
+    license=license,
+)
+
+del os.environ['PYTHONDONTWRITEBYTECODE']
